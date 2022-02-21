35120b7a1b6e72fa7e4aa09a8259a909ff945651
ian <imulet@vols.utk.edu>
2022-02-20 20:53:45 -0500

    added semaphore and barrier temp code

--- sbi/src/lock.c
+++ sbi/src/lock.c
@@ -22,3 +22,46 @@ void mutex_spinlock(Mutex *mutex){
     //spin until unlocked
     while (!mutex_trylock(mutex));
 }
+
+u64 semaphore_trydown(Semaphore *sem) {
+    int old;
+    asm volatile ("amoadd.w %0, %1, (%2)" : "=r"(old) : "r"(-1), "r"(&sem->value));
+    if (old <= 0) {
+        //we need to get back to 0 if the semaphore was negative
+        semaphore_tryup(sem);
+    }
+    return old > 0;
+}
+void semaphore_tryup(Semaphore *sem) {
+    asm volatile ("amoadd.w zero, %0, (%1)" : : "r"(1), "r"(&sem->value));
+}
+
+
+/* void barrier_init(struct Barrier *barrier) { */
+/*     barrier->head = NULL; */
+/*     barrier->value = 0; */
+/* } */
+/* void barrier_add_process(struct Barrier *barrier, struct Process *proc) { */
+/*     struct BarrierList *bl; */
+/*     bl = kmalloc(sizeof(*bl)); */
+/*     bl->proc = proc; */
+/*     bl->next = barrier->head; */
+/*     barrier->head = bl; */
+/*     barrier->value += 1; */
+/* } */
+/* void barrier_at(struct Barrier *barrier) { */
+/*     int old; */
+/*     asm volatile("amoadd.w %0, %1, (%2)"  */
+/*                    : "=r"(old)  */
+/*                    : "r"(-1), "r"(&barrier->value) */
+/*                 ); */
+/*     if (old <= 1) { */
+/*         struct BarrierList *bl, *next; */
+/*         for (bl = barrier->head;NULL != bl;bl = next) { */
+/*             next = bl->next; */
+/*             bl->proc->state = STATE_RUNNING; */
+/*             kfree(bl); */
+/*         } */
+/*         barrier->head = NULL; */
+/*     } */
+/* } */
--- src/include/lock.h
+++ src/include/lock.h
@@ -4,14 +4,29 @@
 #include <common.h>
 
 typedef u64 Mutex;
-typedef u64 Semaphore;
 
+typedef struct Semaphore{
+    s64 value;
+}Semaphore;
+
+/* typedef struct BarrierList { */
+/*     struct Process *proc; */
+/*     struct BarrierList *next; */
+/* }BarrierList; */
+/*  */
+/* typedef struct Barrier { */
+/*     struct BarrierList *head; */
+/*     int value; */
+/* }Barrier; */
 
 u64 mutex_trylock(Mutex *mutex);
 void mutex_unlock(Mutex *mutex);
 u64 semaphore_trydown(Semaphore *sem);
 void semaphore_tryup(Semaphore *sem);
 void mutex_spinlock(Mutex *mutex);
+/* void barrier_init(Barrier*); */
+/* void barrier_add_process(Barrier*, Proc*); */
+/* void barrier_at(Barrier*); */
 
 
 #endif
